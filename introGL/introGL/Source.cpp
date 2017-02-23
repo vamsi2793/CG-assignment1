35f0d0f66619106a2f8b986bae9aa5bfb71f783a
vamsi2793 <vamsi2793@gmail.com>
2017-02-23 13:18:58 +0530

    transformation by keyboard input
    created a wire cone along with a quadrilateral made from triangles.
    Translated only the cone and rotated it along the x/y axis

--- introGL/introGL/Source.cpp
+++ introGL/introGL/Source.cpp
@@ -3,7 +3,7 @@
 #else
 #include <GL/glut.h>
 #endif
-
+int r=0;
 void changeSize(int w, int h) {
 
 	// Prevent a divide by zero, when window is too short
@@ -32,16 +32,36 @@ void changeSize(int w, int h) {
 void renderScene(void) {
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glBegin(GL_LINE_LOOP);
+	glPointSize(5);
+	glBegin(GL_TRIANGLES);
+	    glColor3f(1,0,0);
 		glVertex3f(-2,-2,-5.0);
 		glVertex3f(2,0.0,-5.0);
 		glVertex3f(0.0,2,-5.0);
+		glColor3f(0,1,0);
+		glVertex3f(-2,-2,-5.0);
+		glVertex3f(0.0,2,-5.0);
+		glVertex3f(-2,1,-5.0);
 	glEnd();
-
+	glPushMatrix();
+	//glLoadIdentity();
+	glTranslatef(0,0,-5);
+	glRotatef(10*r,0,1,0);
+	//glLoadIdentity();
+	//glTranslatef(0,0,-5);
+	glColor3f(0,0,1);
+	glutWireCone(1,2,10,3);
+	glPopMatrix();
+	//glTranslatef(0,0,-5);
 	glutSwapBuffers();
 }
-
+void keyboard(unsigned char key,int x, int y)
+{
+	if(key=='j')
+		{r++;
+	glutPostRedisplay();
+	}
+}
 int main(int argc, char **argv) {
 
 	// init GLUT and create window
@@ -53,6 +73,7 @@ int main(int argc, char **argv) {
 	
 
 	// register callbacks
+	glutKeyboardFunc(keyboard);
 	glutDisplayFunc(renderScene);
 	glutReshapeFunc(changeSize);
 
